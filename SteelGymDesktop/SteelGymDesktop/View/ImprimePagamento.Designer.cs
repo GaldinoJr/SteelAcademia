7d88f2d7f08ffb8abcc4f24097ea0ca73ae105fc
GaldinoJr <galdinodbz@gmail.com>
2019-04-06 02:50:12 -0300

    added the print payment view.

---
+++ SteelGymDesktop/SteelGymDesktop/View/ImprimePagamento.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace SteelGymDesktop.View
+{
+    partial class ImprimePagamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImprimePagamento));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboAlunos = new System.Windows.Forms.ComboBox();
+            this.dtpDataPagamento = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalvar2 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.rbNaoPago = new System.Windows.Forms.RadioButton();
+            this.rbPago = new System.Windows.Forms.RadioButton();
+            this.PaymentId = new System.Windows.Forms.MaskedTextBox();
+            this.btnComprovante = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(110, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Valor (R$):";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(208, 93);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(98, 20);
+            this.txtValor.TabIndex = 2;
+            this.txtValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(144, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Aluno:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(151, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Data:";
+            // 
+            // cboAlunos
+            // 
+            this.cboAlunos.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cboAlunos.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboAlunos.FormattingEnabled = true;
+            this.cboAlunos.Location = new System.Drawing.Point(208, 65);
+            this.cboAlunos.Name = "cboAlunos";
+            this.cboAlunos.Size = new System.Drawing.Size(171, 21);
+            this.cboAlunos.Sorted = true;
+            this.cboAlunos.TabIndex = 3;
+            // 
+            // dtpDataPagamento
+            // 
+            this.dtpDataPagamento.CustomFormat = "dd/MM/YYYY";
+            this.dtpDataPagamento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataPagamento.Location = new System.Drawing.Point(208, 120);
+            this.dtpDataPagamento.Name = "dtpDataPagamento";
+            this.dtpDataPagamento.Size = new System.Drawing.Size(98, 20);
+            this.dtpDataPagamento.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(43)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.btnComprovante);
+            this.panel1.Controls.Add(this.btnSalvar2);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Controls.Add(this.rbNaoPago);
+            this.panel1.Controls.Add(this.rbPago);
+            this.panel1.Controls.Add(this.dtpDataPagamento);
+            this.panel1.Controls.Add(this.cboAlunos);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtValor);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(191, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 280);
+            this.panel1.TabIndex = 19;
+            // 
+            // btnSalvar2
+            // 
+            this.btnSalvar2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(21)))), ((int)(((byte)(86)))));
+            this.btnSalvar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar2.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar2.Location = new System.Drawing.Point(208, 226);
+            this.btnSalvar2.Name = "btnSalvar2";
+            this.btnSalvar2.Size = new System.Drawing.Size(155, 30);
+            this.btnSalvar2.TabIndex = 137;
+            this.btnSalvar2.Text = "Salvar";
+            this.btnSalvar2.UseVisualStyleBackColor = false;
+            this.btnSalvar2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(110)))), ((int)(((byte)(140)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(369, 226);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(155, 30);
+            this.btnCancel.TabIndex = 136;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(110, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Foi Pago?";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(21)))), ((int)(((byte)(86)))));
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(369, 226);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(155, 30);
+            this.btnSalvar.TabIndex = 21;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // rbNaoPago
+            // 
+            this.rbNaoPago.AutoSize = true;
+            this.rbNaoPago.ForeColor = System.Drawing.Color.White;
+            this.rbNaoPago.Location = new System.Drawing.Point(275, 167);
+            this.rbNaoPago.Name = "rbNaoPago";
+            this.rbNaoPago.Size = new System.Drawing.Size(73, 17);
+            this.rbNaoPago.TabIndex = 6;
+            this.rbNaoPago.TabStop = true;
+            this.rbNaoPago.Text = "Não Pago";
+            this.rbNaoPago.UseVisualStyleBackColor = true;
+            // 
+            // rbPago
+            // 
+            this.rbPago.AutoSize = true;
+            this.rbPago.ForeColor = System.Drawing.Color.White;
+            this.rbPago.Location = new System.Drawing.Point(208, 167);
+            this.rbPago.Name = "rbPago";
+            this.rbPago.Size = new System.Drawing.Size(50, 17);
+            this.rbPago.TabIndex = 5;
+            this.rbPago.TabStop = true;
+            this.rbPago.Text = "Pago";
+            this.rbPago.UseVisualStyleBackColor = true;
+            // 
+            // PaymentId
+            // 
+            this.PaymentId.Location = new System.Drawing.Point(70, 297);
+            this.PaymentId.Name = "PaymentId";
+            this.PaymentId.ReadOnly = true;
+            this.PaymentId.Size = new System.Drawing.Size(98, 20);
+            this.PaymentId.TabIndex = 20;
+            this.PaymentId.Visible = false;
+            // 
+            // btnComprovante
+            // 
+            this.btnComprovante.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnComprovante.Enabled = false;
+            this.btnComprovante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComprovante.ForeColor = System.Drawing.Color.MintCream;
+            this.btnComprovante.Location = new System.Drawing.Point(208, 226);
+            this.btnComprovante.Name = "btnComprovante";
+            this.btnComprovante.Size = new System.Drawing.Size(155, 30);
+            this.btnComprovante.TabIndex = 138;
+            this.btnComprovante.Text = "Imprimir o Comprovante";
+            this.btnComprovante.UseVisualStyleBackColor = false;
+            this.btnComprovante.Visible = false;
+            // 
+            // ImprimePagamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(37)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(899, 651);
+            this.Controls.Add(this.PaymentId);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ImprimePagamento";
+            this.Text = "Cadastro de Movimentacao";
+            this.Load += new System.EventHandler(this.ImprimePagamento_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox txtValor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboAlunos;
+        private System.Windows.Forms.DateTimePicker dtpDataPagamento;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.RadioButton rbNaoPago;
+        private System.Windows.Forms.RadioButton rbPago;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox PaymentId;
+        private System.Windows.Forms.Button btnSalvar2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnComprovante;
+    }
+}
---
+++ SteelGymDesktop/SteelGymDesktop/View/ImprimePagamento.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Windows.Forms;
+using SteelGymDesktop.Applications.Interfaces;
+using SteelGymDesktop.Domain.Entities;
+using System.Collections.Generic;
+using SteelGymDesktop.Models;
+using System.Globalization;
+
+namespace SteelGymDesktop.View
+{
+    public partial class ImprimePagamento : Form
+    {
+        private readonly IStudentAppService _studentApp;
+        private readonly IPaymentAppService _paymentApp;
+        private bool _isCreate;
+        private int _pagamentoId;
+        private Payment _payment;
+        public IEnumerable<Student> students;
+
+        public ImprimePagamento(
+            IPaymentAppService paymentApp,
+            IStudentAppService studentApp,
+            bool isCreate, 
+            int pagamentoId
+        )
+        {
+            _paymentApp = paymentApp;
+            _studentApp = studentApp;
+
+           
+            this._isCreate = isCreate;
+            this._pagamentoId = pagamentoId;
+            InitializeComponent();
+            
+            var students = _studentApp.GetAllActive();
+            cboAlunos.Items.Clear();
+            if (students != null)
+            {
+                foreach (var student in students)
+                {
+                    cboAlunos.Items.Add(new ComboItem(student.Name, student.StudentId));
+                }
+            }
+        }
+
+        private void ImprimePagamento_Load(object sender, EventArgs e)
+        {
+            if (_pagamentoId != int.MinValue)
+            {
+                LoadPayment(_pagamentoId);
+            }
+            if(_isCreate)
+            {
+                btnSalvar.Visible = true;
+                btnSalvar.Enabled = true;
+                btnSalvar2.Visible = false;
+                btnSalvar2.Enabled = false;
+                btnCancel.Visible = false;
+                btnCancel.Enabled = false;
+            } else
+            {
+                btnSalvar.Visible = false;
+                btnSalvar.Enabled = false;
+                btnSalvar2.Visible = _payment.FgPago != 1;
+                btnSalvar2.Enabled = _payment.FgPago != 1;
+                btnCancel.Visible = true;
+                btnCancel.Enabled = true;
+
+                btnComprovante.Visible = _payment.FgPago == 1;
+                btnComprovante.Enabled = _payment.FgPago == 1;
+
+
+                cboAlunos.Enabled = false;
+                txtValor.Enabled = false;
+                dtpDataPagamento.Enabled = false;
+                rbNaoPago.Enabled = rbPago.Enabled = _payment.FgPago != 1;
+            }
+        }
+
+        private void LoadPayment(int pagamentoId)
+        {
+            try
+            {
+
+                PaymentId.Text = pagamentoId.ToString();
+                _payment = _paymentApp.GetById(pagamentoId);
+                txtValor.Text = _payment.Value.ToString();
+                var students = _studentApp.GetAllActive();
+                cboAlunos.Items.Clear();
+                if (students != null)
+                {
+                    int index = 0;
+                    foreach (var student in students)
+                    {
+                        cboAlunos.Items.Add(new ComboItem(student.Name, student.StudentId));
+
+                        if (_payment.StudentId.ToString() == student.StudentId.ToString())
+                        {
+                            cboAlunos.SelectedIndex = index;
+                        }
+
+                        index++;
+                    }
+                }
+                
+                dtpDataPagamento.Value = Convert.ToDateTime(_payment.DataPagamento);
+                if(Convert.ToBoolean(_payment.FgPago))
+                {
+                    rbNaoPago.Checked = false;
+                    rbPago.Checked = true;
+                }
+                else
+                {
+                    rbNaoPago.Checked = true;
+                    rbPago.Checked = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Util.ShowMessageWarning(ex.Message);
+            }
+
+        }
+
+        private void DisableScreen()
+        {
+            txtValor.Enabled =
+            cboAlunos.Enabled =
+            dtpDataPagamento.Enabled =
+            rbNaoPago.Enabled =
+            rbPago.Enabled =
+            btnSalvar.Enabled = false;
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msgError = "";
+
+                if (!ValidateFields(out msgError))
+                {
+                    Util.ShowMessageWarning(msgError);
+                    return;
+                }
+                if (_isCreate)
+                {
+                    _payment = new Payment();
+                }
+
+                _payment.Value = Convert.ToDecimal(txtValor.Text.ToString().Replace(",", ".").Replace(" ",""), new CultureInfo("en-US"));
+                _payment.StudentId = (int) ((ComboItem)cboAlunos.SelectedItem).Value;
+                _payment.DataPagamento = dtpDataPagamento.Text;
+
+                if (rbPago.Checked)
+                    _payment.FgPago = 1;
+                else
+                    _payment.FgPago = 0;
+
+                if (_isCreate)
+                {
+                    Util.DisabledCursor();
+
+                    _paymentApp.Add(_payment);
+                    Util.ShowMessageWarning("Pagamento inserido com sucesso.");
+                    Limpar();
+
+                    Util.EnabledCursor();
+
+                    ImprimePagamento p = new ImprimePagamento(_paymentApp, _studentApp, false, _payment.PaymentId);
+                    p.ShowDialog();
+                }
+                else
+                {
+                    Util.DisabledCursor();
+
+                    _payment.PaymentId = Convert.ToInt32(PaymentId.Text);
+                    _paymentApp.Update(_payment);
+                    Util.ShowMessageWarning("Pagamento alterado com sucesso.");
+
+                    Util.EnabledCursor();
+
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Util.ShowMessageWarning(ex.Message);
+            }
+        }
+
+        private void Limpar()
+        {
+            txtValor.Text = "";
+            cboAlunos.Text = "";
+            dtpDataPagamento.Text = "";
+            rbNaoPago.Checked = true;
+            rbPago.Checked = true;
+        }
+
+        private bool ValidateFields(out string msgError)
+        {
+            msgError = "";
+            bool pass = true;
+            if (!Util.ValidaString(txtValor.Text))
+            {
+                msgError += " - Campo 'Valor R$' invalido.";
+                if (pass)
+                {
+                    txtValor.Focus();
+                }
+                pass = false;
+            }
+            if (cboAlunos.SelectedIndex == -1)
+            {
+                msgError += " - Campo 'Aluno' invalido.";
+                if (pass)
+                {
+                    cboAlunos.Focus();
+                }
+                pass = false;
+            }
+            if (!Util.ValidaData(dtpDataPagamento.Value.Date))
+            {
+                msgError += " - Campo 'Data do Pagamento' invalido.";
+                if (pass)
+                {
+                    dtpDataPagamento.Focus();
+                }
+                pass = false;
+            }
+            if(rbPago.Checked == false && rbNaoPago.Checked == false)
+            {
+                msgError += " - Campo 'Pago?' invalido.";
+                if (pass)
+                {
+                    dtpDataPagamento.Focus();
+                }
+                pass = false;
+            }
+            return pass;
+        }
+
+        private void txtValor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+                e.Handled = true;
+
+            if ((e.KeyChar == ',') && (((MaskedTextBox)sender).Text.IndexOf(',') > -1))
+                e.Handled = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BtnSalvar_Click(sender, e);
+        }
+    }
+}
